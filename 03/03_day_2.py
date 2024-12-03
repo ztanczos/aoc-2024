d21dfd054829fc9fc8e7190c7acf4e247d4e45ba
Zoltan Tanczos <ztanczos@gmail.com>
2024-12-03 12:45:38 +0100

    Remove unnecessary prints from day 3

--- 03/03_day_2.py
+++ 03/03_day_2.py
@@ -7,7 +7,6 @@ with open("03_input.txt", "r") as f:
         memory = line.strip()
         instructions = [(m.start(0), [int(m.group(1)), int(m.group(2))]) for m in re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', memory)]
         startStop = [(m.start(0), m.group(0)) for m in re.finditer(r'(do\(\)|don\'t\(\))', memory)]
-        print(startStop)
         i = 0
         for instruction in instructions:
             if len(startStop) > i and instruction[0] > startStop[i][0]:
@@ -19,6 +18,5 @@ with open("03_input.txt", "r") as f:
 
             if enabled:
                 result += instruction[1][0] * instruction[1][1]
-            print(f'enabled: {enabled}, index: {instruction[0]}: {instruction[1][0]}*{instruction[1][1]}')
 
     print(result)
