89f470b90ea75f58951596f4005a982b829a1c3e
Zoltan Tanczos <ztanczos@gmail.com>
2024-12-05 09:34:16 +0100

    Simplify for loop by removing unnecessary bool flag for Day 5 part 1

--- 05/05_day_1.py
+++ 05/05_day_1.py
@@ -8,17 +8,15 @@ with open("05_input.txt", "r") as f:
     result = 0
     for line in f:
         updates = [int(i) for i in line.strip().split(',')]
-        good = True
         for u, update in enumerate(updates):
             for rule in rules:
                 if rule[0] == update:
                     if rule[1] in updates and updates.index(rule[1]) <= u:
-                        good = False
                         break
             else:
                 continue
             break
-        if good:
+        else:
             result += updates[(len(updates) // 2)]
 
 print(result)
