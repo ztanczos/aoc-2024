ff70b46f632ce254a932dc5610d269c1b46ca160
Zoltan Tanczos <ztanczos@gmail.com>
2024-12-08 07:31:52 +0100

    Remove unnecessary add/mul functions and use operator.add/mul

--- 07/07_day_2.py
+++ 07/07_day_2.py
@@ -1,12 +1,6 @@
 import itertools
+import operator
 from functools import reduce
-
-
-def mul(lval: int, rval: int) -> int:
-    return lval * rval
-
-def add(lval: int, rval: int) -> int:
-    return lval + rval
 
 def concat(lval: int, rval: int) -> int:
     return int(str(lval) + str(rval))
@@ -21,7 +15,7 @@ with open("07_input.txt", "r") as f:
         calibrations.append(equation)
 
 for target, numbers in calibrations:
-    for operations in itertools.product([mul, add, concat], repeat=len(numbers) - 1):
+    for operations in itertools.product([operator.mul, operator.add, concat], repeat=len(numbers) - 1):
         computed_value = reduce(lambda acc, func_num: func_num[0](acc, func_num[1]), zip(operations, numbers[1:]), numbers[0])
         if computed_value == target:
             result += computed_value
